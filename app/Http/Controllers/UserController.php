17729875c3d58bb4cfb437d430a1aeb7ea8fcf3d
k.nagasaka <kentaro7939@gmail.com>
2021-09-24 10:26:25 +0900

    ユーザー編集機能を追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,23 @@ use App\Models\User;
 class UserController extends Controller
 {
     //
-    public function index() {
+    public function index()
+    {
         $users = User::all();
         return view('user/index', compact('users'));
+    }
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('user/edit', compact('user'));
+    }
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+        return redirect('/user');
     }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>User一覧</title>
-
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-</head>
+@extends('user/layout')
+@section('content')
 
 <body>
     <div class="container ops-main">
@@ -53,4 +43,4 @@
         </div>
 </body>
 
-</html>
+@endsection
