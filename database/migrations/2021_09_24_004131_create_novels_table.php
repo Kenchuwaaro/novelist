8e8fcdf955e9109a76c77fe503262525a2ad6d65
k.nagasaka <kentaro7939@gmail.com>
2021-09-24 09:44:04 +0900

    novelsテーブルを追加

---
+++ database/migrations/2021_09_24_004131_create_novels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNovelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('novels', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 1028);
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->foreign('user_id')
+            ->references('id')->on('users')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('novels');
+    }
+}
